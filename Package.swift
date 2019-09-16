26a93db01de0627c21b6bb30305777204c85d7e4
David Knezić <davidknezic@gmail.com>
2019-09-16 16:33:40 +0200

    Fix target name

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         .library(
             name: "SwiftTor",
-            targets: ["Tor"]),
+            targets: ["SwiftTor"]),
         .library(
             name: "Ctor",
             targets: ["Ctor"]),
