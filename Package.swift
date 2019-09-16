1a4523bc12fe619e9c279dcf6e2ce39bb0726387
David Knezić <davidknezic@gmail.com>
2019-09-16 16:21:36 +0200

    Rename

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Tor",
+    name: "SwiftTor",
     products: [
         .library(
             name: "Tor",
--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,5 +1,4 @@
 import XCTest
-
 import TorTests
 
 var tests = [XCTestCaseEntry]()
